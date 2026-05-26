ddec8fa8aa37751c543bccd6976d757d3acdda41
Ehsan Moradi <ehsan.hightech@gmail.com>
2026-05-26 14:19:02 +0330

    update overflow size in empty chat page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    //allowedDevOrigins: ["188.136.214.220:3000","188.136.214.220:8000","188.136.214.220"],
 };
 
 export default nextConfig;
